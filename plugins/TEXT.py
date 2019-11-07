98d5157efcad73704daa44f32bcef1510f36db48
scooby143 <51952881+scooby143@users.noreply.github.com>
2019-11-07 19:54:21 +0530

    Update TEXT.py

--- plugins/TEXT.py
+++ plugins/TEXT.py
@@ -9,27 +9,27 @@ MEGA_EMAIL = "rpdrive@hi2.in"
 MEGA_PASSWORD = "rpdrive@hi2.in"
 
 
-START = " Hi {}  \nI am Drive Uploader Bot and I will Uploads files to your Google Drive from URLs.\nPlease Authorise to use me by using /auth "
+START = " Hi {}  \n\nI am Drive Uploader Bot and I will Uploads files to your Google Drive from URLs.\n\nPlease Authorise to use me by using /auth "
 
 
 DP_DOWNLOAD = "Dropbox Link !! Downloading Started ..."
-OL_DOWNLOAD = "Openload Link !! Downloading Started ... \n Openload Links Are Extremely Slow"
-PROCESSING = "Processing Your Request ...!!"
+OL_DOWNLOAD = "Openload Link !! Downloading Started ... \n\nOpenload Links are Extremely Slow"
+PROCESSING = "Processing your Request ...!!"
 DOWN_TWO = True
 DOWNLOAD = "Downloading Started ..."
-DOWN_MEGA = "Downloading Started... \n  Mega Links are \n Extremely Slow :("
-DOWN_COMPLETE = "Downloading complete !!"
-NOT_AUTH = "You Are Not Authorised To Using this Bot \n\n Please Authorise Me Using /auth "
-REVOKE_FAIL = "You are Already UnAuthorised \n. Please use /auth to Authorise "
-AUTH_SUCC = "Authorised Successfully  !! \n\n Now Send me a Direct Link :)"
-ALREADY_AUTH = "You Are Already Authorised ! \n\n Wanna Change Drive Account? \n\n Use /revoke "
-AUTH_URL = '<a href ="{}">Vist This Url</a> \n Generate And Copy Your Google Drive Token And Send It To Me'
-UPLOADING = "Download Complete !! \n Uploading Your file"
-REVOKE_TOK = " Your Token is Revoked Successfully !! \n\n Use /auth to Re-Authorise your Drive Account. "
+DOWN_MEGA = "Downloading Started... \n\nMega Links are Extremely Slow "
+DOWN_COMPLETE = "Downloading Complete !!"
+NOT_AUTH = "You are not Authorised to use this Bot \n\nPlease Authorise me by using /auth "
+REVOKE_FAIL = "You are Already UnAuthorised.\n\nPlease use /auth to Authorise "
+AUTH_SUCC = "Authorised Successfully  !! \n\nNow Send me a Direct Link to Upload "
+ALREADY_AUTH = "You are already Authorised ! \n\nWanna change Drive Account use /revoke "
+AUTH_URL = 'To Authorise your Account \n\n<a href ="{}">Vist this URL</a> \n\nGenerate and Copy your Google Drive Token and Send it to me'
+UPLOADING = "Download Complete !! \nUploading Your file"
+REVOKE_TOK = " Your Token is Revoked Successfully !! \n\nUse /auth to Re-Authorise your Drive Account"
 # DOWN_PATH = "Downloads\\" #windows path
 DOWN_PATH = "Downloads/"  # Linux path
-DOWNLOAD_URL = "Your File Uploaded Successfully \n\n <b>Filename</b> : {} \n\n <b> Size</b> : {} MB \n\n <b>Download</b> {}"
-AUTH_ERROR = "AUTH Error !! Please  Send Me a  valid Token or Re - Authorise Me "
+DOWNLOAD_URL = "Your File Uploaded Successfully \n\n <b>Filename</b> : {} \n\n <b>Size</b> : {} MB \n\n <b>Download</b> {}"
+AUTH_ERROR = "AUTH Error !! Please Send me a valid Token or Re-Authorise"
 OPENLOAD = True
 DROPBOX = True
 MEGA = True
